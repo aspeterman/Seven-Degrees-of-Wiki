c368856beac69e23db1694a5755cf841e8d272c2
aspeterman <austin.s.peterman@gmail.com>
2020-02-28 11:23:39 -0700

    start&end objectives click counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import SearchList from './components/SearchList'
 import Form from './components/Form'
-import Random from './components/Random'
 import GetStarted from './components/GetStarted';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data : []
+      data : [],
+      page: ''
     };
     this.normalizeData = (rawData) => {
       return rawData[1].map(function(title, index) {
@@ -35,11 +35,10 @@ class App extends React.Component {
     return (
       <div className='container'>
         <h1>Kevin Bacon's 7 Degrees of Freedom</h1>
-        <GetStarted/>
-        <a ref="noopener noreferrer" target ="_blank" href="https://en.wikipedia.org/wiki/Special:Random"><i className="fa fa-random" aria-hidden="true"></i>Search</a>
+        {/* <a ref="noopener noreferrer" target ="_blank" href="https://en.wikipedia.org/wiki/Special:Random"><i className="fa fa-random" aria-hidden="true"></i>Search</a> */}
         <Form onChange={this.addNewResult} />
-        <Random onClick={this.setStartPage}/>
         <SearchList query={this.state.data} />
+        <GetStarted/>
       </div>
     )
   }
--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,54 +1,73 @@
 // import axios from 'axios'
 // import React, {Component} from 'react'
+import countClicks from '../helpers/countClicks'
 var axios = require('axios')
 var React = require('react');
-
 class GetStarted extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       start: ''
     };
-    this.props = this.start
   }
 
-
-  getStartingPage() {
+async handleClick() {
+  axios.all([
     axios.get("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=mapdata&list=random", {
     params: {
       // action: '',
-      datatype: 'json',
+      datatype: 'jsonp',
+      // limit: 15,
+      origin: '*'
+    }
+  }),
+  axios.get("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=mapdata&list=random", {
+    params: {
+      // action: '',
+      datatype: 'jsonp',
       // limit: 15,
       origin: '*'
     }
   })
-  .then((resp) => {
-    // this.props.onClick(resp.data);
-    var start = resp.data.query.random[0].title
+  ])
+  .then(axios.spread((firstCall, secondCall) => {
+    var start = firstCall.data.query.random[0].title
     document.getElementById('origin').innerHTML = start
-  })
-}
+    var end = secondCall.data.query.random[0].title
+    document.getElementById('end').innerHTML = end })
+  )}
 
-renderStartingPage() {
-  axios.get(`https://en.wikipedia.org/w/api.php?action=parse&format=json&title=hello&text=%7B%7BProject%3ASandbox%7D%7D&prop=text&contentmodel=wikitext`, {
-    params: {
-      datatype: 'json',
-      origin: '*'
-    }
-  })
-  .then(response => response)
-  .then(data => {
-    const resData = Object.values(data.data.parse.text);
-    console.log(resData);
-    // console.log(data.data.parse.text)
-    document.getElementById('app').innerHTML = `Starting at: ${resData}`;
-  })
-}
+  renderStartingPage() {
+    axios.get(`https://en.wikipedia.org/w/api.php?action=parse&format=json&page=${document.getElementById('origin').innerText}`, {
+    // axios.get(`https://en.wikipedia.org/w/api.php?action=parse&format=json&title=hello&text=%7B%7BProject%3ASandbox%7D%7D&prop=text&contentmodel=wikitext`, {
+      params: {
+        datatype: 'jsonp',
+        origin: '*'
+      }
+    })
+    .then(response => response)
+    .then(data => {
+      const resData = Object.values(data.data.parse.text);
+      console.log(resData);
+      document.getElementById('wiki').innerHTML = resData;
+    })
+  }
+
   render(){
     return(
       <div>
-        <button onClick={this.getStartingPage}>Get Started</button>
+        <button onClick={this.handleClick}>Get Your Objective</button>
         <button onClick={this.renderStartingPage}>Get Rendered</button>
+        <div>
+          <span>Starting Point:</span>
+          <span id="origin"></span>
+        </div>
+        <div>
+          <span>Ending Point:</span>
+          <span id="end"></span>
+        </div>
+        <span id="counter"></span>
+        <div id="wiki" onClick={countClicks}></div>
       </div>
     )
   }
